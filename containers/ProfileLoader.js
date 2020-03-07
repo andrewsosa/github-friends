3ca1e4cf8b130929294adc63fc7f1014610d4677
Andrew Sosa <andrew@andrewsosa.com>
2020-03-07 12:47:11 -0500

    Add goal tracking to adding friends

--- containers/ProfileLoader.js
+++ containers/ProfileLoader.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from "react";
+import * as Fathom from "fathom-client";
 import useFollow from "../hooks/follow";
 import Profile from "../components/Profile";
 import { useProfile } from "../hooks/profile";
@@ -17,10 +18,10 @@ const ProfileLoader = ({ username, source }: IProfileLoader) => {
   const sourceData = useProfile(source);
 
   // This is what powers the follow button
-  const followCallback = React.useCallback(() => follow(username), [
-    follow,
-    username,
-  ]);
+  const followCallback = React.useCallback(() => {
+    Fathom.trackGoal("ORRJAV2A", 0);
+    follow(username);
+  }, [follow, username]);
 
   return (
     <>
